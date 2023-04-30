b72efc10c4ab38d298cc6f7b9d0033f91df0659e
Dmitriy <35215775+DimaTer89@users.noreply.github.com>
2023-04-30 15:55:51 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace RemoveMaxNumberEdgesKeepGraphFullyTraversable
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
